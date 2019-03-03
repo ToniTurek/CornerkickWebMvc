14f37d4b77553fd5e1f7caf395db5145de6c930b
Jan Suhrmann <s.jan@web.de>
2019-03-03 13:24:25 +0100

    Correct check for 'migrationHistoryTableExists'

--- MySqlInitializer.cs
+++ MySqlInitializer.cs
@@ -15,7 +15,7 @@ namespace CornerkickWebMvc
       } else {
         // query to check if MigrationHistory table is present in the database 
         var migrationHistoryTableExists = ((IObjectContextAdapter)context).ObjectContext.ExecuteStoreQuery<int>(
-          "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = 'IdentityMySQLDatabase' AND table_name = '__MigrationHistory'");
+          "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = 'ckmysqldb' AND table_name = '__MigrationHistory'");
 
         // if MigrationHistory table is not there (which is the case first time we run) - create it
         if (migrationHistoryTableExists.FirstOrDefault() == 0) {
