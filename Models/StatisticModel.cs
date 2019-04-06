e45911c26a0fef71aabbac809aa28b870eb28f45
Jan Suhrmann <s.jan@web.de>
2019-04-06 15:58:25 +0200

    Add missing StatisticModel.cs

---
+++ Models/StatisticModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CornerkickWebMvc.Models
+{
+  public class StatisticModel
+  {
+    public string sFormation { get; set; }
+    public List<SelectListItem> ltsFormations { get; set; }
+  }
+}
