806ab25d77a292eca2b7df4bce31b060dcb8058b
Jan Suhrmann <s.jan@web.de>
2019-08-20 20:00:15 +0200

    Update user manual

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -29,11 +29,12 @@ namespace CornerkickWebMvc.Controllers
       return View();
     }
 
-    public ActionResult UserManual()
+    public ActionResult UserManual(Models.UserManualModel mdUm)
     {
       ViewBag.Message = "Cornerkick User Manual";
 
-      return View();
+
+      return View(mdUm);
     }
 
     private CornerkickManager.User ckUser()
@@ -92,5 +93,76 @@ namespace CornerkickWebMvc.Controllers
 
       return Content(JsonConvert.SerializeObject(dataPoints, _jsonSetting), "application/json");
     }
+
+    public ContentResult UmGetPlayerTraining(int iType, int iTrainer, int iCamp, int iAge)
+    {
+      List<Models.DataPointGeneral>[] dataPoints = new List<Models.DataPointGeneral>[3];
+
+      // Initialize dataPoints list
+      for (byte j = 0; j < dataPoints.Length; j++) dataPoints[j] = new List<Models.DataPointGeneral>();
+
+      // Create temp. player
+      CornerkickManager.Main mnUm = new CornerkickManager.Main();
+      mnUm.dtDatum = mnUm.dtDatum.AddDays(1);
+
+      CornerkickManager.User usr = new CornerkickManager.User();
+      mnUm.ltUser.Add(usr);
+
+      // Create Club
+      CornerkickManager.Club clb = new CornerkickManager.Club();
+      clb.iId = 0;
+      clb.sName = "Team";
+      clb.iLand = 0;
+      clb.iDivision = 0;
+      clb.user = usr;
+      for (byte iT = 0; iT < clb.training.iType.Length; iT++) clb.training.iType[iT] = (byte)iType; // Condition
+      clb.staff.iCondiTrainer = 4;
+      mnUm.ltClubs.Add(clb);
+
+      usr.club = clb;
+
+      CornerkickGame.Player pl = new CornerkickGame.Player();
+      pl.fCondition = 0.6f;
+      pl.fFresh = 0.8f;
+      pl.fMoral = 1.0f;
+      pl.iClubId = 0;
+      pl.dtBirthday = mnUm.dtDatum.AddYears(-iAge);
+      clb.ltPlayer.Add(pl);
+
+      clb.staff.iCondiTrainer = (byte)iTrainer;
+      //pl.doDoping(mn.ltDoping[1]);
+
+      CornerkickManager.TrainingCamp.Booking camp = new CornerkickManager.TrainingCamp.Booking();
+      foreach (CornerkickManager.TrainingCamp.Camp cp in mnUm.tcp.ltCamps) {
+        if (cp.iId == iCamp) {
+          camp.camp = cp;
+          camp.dtArrival   = mnUm.dtDatum.AddDays(-1);
+          camp.dtDeparture = mnUm.dtDatum.AddDays(+8);
+          break;
+        }
+      }
+
+      // For the next 7 days ...
+      for (byte iD = 0; iD < 7; iD++) {
+        DateTime dtTmp = mnUm.dtDatum.AddDays(iD);
+
+        if (iD > 0) {
+          if ((int)dtTmp.DayOfWeek == 0) break;
+
+          // ... do training
+          mnUm.plr.doTraining(ref pl, dtTmp, camp, false, true);
+        }
+
+        // ... add training data to dataPoints
+        dataPoints[0].Add(new Models.DataPointGeneral(iD + 1, pl.fCondition));
+        dataPoints[1].Add(new Models.DataPointGeneral(iD + 1, pl.fFresh));
+        dataPoints[2].Add(new Models.DataPointGeneral(iD + 1, pl.fMoral));
+      }
+
+      JsonSerializerSettings _jsonSetting = new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Ignore };
+
+      return Content(JsonConvert.SerializeObject(dataPoints, _jsonSetting), "application/json");
+    }
+
   }
 }
--- Models/UserManualModel.cs
+++ Models/UserManualModel.cs
@@ -2,10 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace CornerkickWebMvc.Models
 {
   public class UserManualModel
   {
+    public string sTraining { get; set; }
+    public List<SelectListItem> ddlTraining { get; set; }
+
+    public UserManualModel()
+    {
+      ddlTraining = new List<SelectListItem>();
+      for (byte i = 0; i < MvcApplication.ckcore.sTraining.Length; i++) {
+        ddlTraining.Add(
+          new SelectListItem {
+            Text = MvcApplication.ckcore.sTraining[i],
+            Value = i.ToString()
+          }
+        );
+      }
+    }
   }
+
 }
