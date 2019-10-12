5ef52b740af983051713c2c73e52630980326b7e
Jan Suhrmann <s.jan@web.de>
2019-10-12 18:47:59 +0200

    Update for new ck lib

--- Controllers/MemberController.cs
+++ Controllers/MemberController.cs
@@ -2344,7 +2344,7 @@ namespace CornerkickWebMvc.Controllers
           strengthIdeal = CornerkickGame.Tool.getAveSkill(transfer.player, bIdeal: true) .ToString("0.0"),
           age = transfer.player.getAge(MvcApplication.ckcore.dtDatum).ToString("0"),
           talent = (transfer.player.iTalent + 1).ToString(),
-          mw = (MvcApplication.ckcore.plr.getValue(transfer.player) * 1000).ToString("N0", getCi()),
+          mw = (transfer.player.getValue(MvcApplication.ckcore.dtDatum) * 1000).ToString("N0", getCi()),
           fixtransferfee = sFixTransferFee,
           club = sClub,
           nat = MvcApplication.ckcore.sLandShort[transfer.player.iNat1]
