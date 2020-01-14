b9fe669124baceeb14337410a22583db876c4df2
Jan Suhrmann <s.jan@web.de>
2020-01-14 17:10:17 +0100

    Fix gold/silver cup tables

--- Scripts/CupGold.js
+++ Scripts/CupGold.js
@@ -5,37 +5,38 @@
     dataType: "JSON",
     data: { iSaison: iSaison, iMatchday: iMd, iGroup: iGroup },
     success: function (sTeams) {
-      actionDrawTeams(sTeams);
+      actionDrawTeamsCupGold(sTeams);
       drawTableCupGold();
     }
   });
 }
 
-function actionDrawTeams(sTeams) {
+function actionDrawTeamsCupGold(sTeams) {
   var divDrawCupTeams = $("#tableDivCupGoldTeams");
   divDrawCupTeams.html('');
   result = drawTeams(sTeams);
   divDrawCupTeams.html(result).show();
 }
 
-function drawTableWc() {
-  var iMd = parseInt($('#ddlMatchdayCupWc').val());
+function drawTableCupGold() {
+  var iSn = parseInt($('#ddlSeasonCupGold').val());
+  var iMd = parseInt($('#ddlMatchdayCupGold').val());
   var iGp = parseInt($('#ddlGroupsCupGold').val());
 
-  var divTableWc = $("#divCupGoldTable");
-  var divScorer = $("#divCupGoldScorer");
+  var divTableCupGold = $("#divCupGoldTable");
+  var divScorerCupGold = $("#divCupGoldScorer");
 
-  divTableWc.html('');
-  divScorer.html('');
+  divTableCupGold.html('');
+  divScorerCupGold.html('');
 
   $.ajax({
     url: '/Member/CupGetLeague',
     type: "GET",
     dataType: "JSON",
-    data: { iCupId: 3, iSaison: 1, iMatchday: iMd, iGroup: iGp },
+    data: { iCupId: 3, iSaison: iSn, iMatchday: iMd, iGroup: iGp },
     success: function (sTable) {
       var sBox = drawTable(sTable);
-      divTableWc.html(sBox).show();
+      divTableCupGold.html(sBox).show();
     }
   });
 }
--- Scripts/CupSilver.js
+++ Scripts/CupSilver.js
@@ -5,37 +5,38 @@
     dataType: "JSON",
     data: { iSaison: iSaison, iMatchday: iMd, iGroup: iGroup },
     success: function (sTeams) {
-      actionDrawTeams(sTeams);
+      actionDrawTeamsCupSilver(sTeams);
       drawTableCupSilver();
     }
   });
 }
 
-function actionDrawTeams(sTeams) {
-  var divDrawCupTeams = $("#tableDivCupSilverTeams");
-  divDrawCupTeams.html('');
+function actionDrawTeamsCupSilver(sTeams) {
+  var divDrawCupSilverTeams = $("#tableDivCupSilverTeams");
+  divDrawCupSilverTeams.html('');
   result = drawTeams(sTeams);
-  divDrawCupTeams.html(result).show();
+  divDrawCupSilverTeams.html(result).show();
 }
 
-function drawTableWc() {
+function drawTableCupSilver() {
+  var iSn = parseInt($('#ddlSeasonCupSilver').val());
   var iMd = parseInt($('#ddlMatchdayCupSilver').val());
   var iGp = parseInt($('#ddlGroupsCupSilver').val());
 
-  var divTableWc = $("#divCupSilverTable");
-  var divScorer = $("#divCupSilverScorer");
+  var divTableCupSilver = $("#divCupSilverTable");
+  var divScorerCupSilver = $("#divCupSilverScorer");
 
-  divTableWc.html('');
-  divScorer.html('');
+  divTableCupSilver.html('');
+  divScorerCupSilver.html('');
 
   $.ajax({
     url: '/Member/CupGetLeague',
     type: "GET",
     dataType: "JSON",
-    data: { iCupId: 4, iSaison: 1, iMatchday: iMd, iGroup: iGp },
+    data: { iCupId: 4, iSaison: iSn, iMatchday: iMd, iGroup: iGp },
     success: function (sTable) {
       var sBox = drawTable(sTable);
-      divTableWc.html(sBox).show();
+      divTableCupSilver.html(sBox).show();
     }
   });
 }
